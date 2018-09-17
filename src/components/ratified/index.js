1a7b5884e3a1534414a5a018a16f2324b75f779c
John Osterman <josterman@pewtrusts.org>
2018-09-17 14:45:55 -0400

    Fixed unused var in tiles

---
+++ src/components/ratified/index.js
@@ -0,0 +1,27 @@
+import s from './styles.scss';
+import Element from '@UI/element/element.js';
+
+export default class RatifiedView extends Element {
+    prerender(args){
+        this.parent = args[2];
+		var div = super.prerender();
+		if ( this.prerendered ) {
+			return div;
+		}
+        div.classList.add(s.ratifiedView, s[this.model.key]);
+        div.innerHTML = `
+                        <div>
+                            <span class="${s.numberRatified}">${this.parent.model.treatiesNested.find(d => d.key === this.model.key).values.length}</span>
+                            Ratified
+                        </div>
+                        <div>
+                            <span class="${s.treatyStatus}${this.model.status === 'Not In Force' ? ' ' + s.invert : '' }">${this.model.status}</span>
+                            Status
+                        </div>
+                        `;
+		return div;
+	}
+    init(){
+        console.log('Init ratified view')
+    }
+}
