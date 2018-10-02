ea55c725057a1348763360d222c673236e9f58a9
John Osterman <josterman@pewtrusts.org>
2018-10-02 16:51:44 -0400

    Set transform back to none on setTimeout instead of on animation finish; FF and others were not timed right

--- src/components/tile/tile.js
+++ src/components/tile/tile.js
@@ -121,8 +121,11 @@ export default class CountryTile {
         
         var delay = document.documentElement.clientWidth > 628 ? 200 : 100; // time in ms it should take for transitions to have begun
         var duration = document.documentElement.clientWidth > 628 ? 500 : 250; // time in ms it should take to complete all transitions
+        setTimeout(() => {
+            this.el.style.transform = 'none';
+        }, delay);
         this.el.style.zIndex = length - index;
-            this.moveTiles = this.el.animate([{
+        this.moveTiles = this.el.animate([{
               transformOrigin: 'top left',
               transform: ( this.deltaY === 0 && this.deltaX === 0 ) ?
                 `translate(${this.deltaX}px, ${this.deltaY}px)` :
@@ -136,8 +139,6 @@ export default class CountryTile {
               delay: ( delay / length ) * index
                 });
         this.moveTiles.onfinish = () => {
-            this.el.style.transform = 'none';
-           // this.el.classList.remove(s.animating);
             if ( index === length - 1){ // ie is the last tile
                 
                 this.parent.tiles.forEach(each => {
