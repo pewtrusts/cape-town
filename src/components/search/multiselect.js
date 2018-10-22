4e7a10291cff3a3e58a08a4409b17ec15dc253e5
John Osterman <josterman@pewtrusts.org>
2018-10-22 09:29:35 -0400

    Fixes e is not defined

--- src/components/search/multiselect.js
+++ src/components/search/multiselect.js
@@ -140,7 +140,7 @@ export default class Multiselect extends Mobius1Selectr {
                     });
                 }
             });
-            button.addEventListener('focus', () => {
+            button.addEventListener('focus', (e) => {
                 e.stopImmediatePropagation();
                 S.setState('selectrTagFocus', true);
             });
