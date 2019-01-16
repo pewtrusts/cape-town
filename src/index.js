9102ac0a5f7ed14291c75ef8de516378013da370
John Osterman <josterman@pewtrusts.org>
2019-01-16 14:37:16 -0500

    Removes DOM check for #prerender-script: no longer relevant

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "StringHelpers" }]*/
-import '@Helpers/string-helpers.js'; // string helpers is an IIFE, invoke when imported but never referenced again
+/* entry point. any polyfills or native prototype extensions should be imported here */
+import '@Helpers/string-helpers.js'; // string helpers is an IIFE
+import './arrayFrom.js'; // IIFE
+
 import CapeTown from './cape-town.js';
-import './arrayFrom.js';
 
 const App = new CapeTown();
 App.wasPrerendered = true;
 const appContainer = document.querySelector('#pew-app');
-const prerenderScript = document.querySelector('#prerender-script');
 if ( !appContainer.classList.contains('rendered') ){
-
 	App.prerender();
-}
-if ( prerenderScript !== null ){ // html for build process includes spript#prerender-script that should be removed on build
-	prerenderScript.parentNode.removeChild(prerenderScript);
 }
 App.init();
